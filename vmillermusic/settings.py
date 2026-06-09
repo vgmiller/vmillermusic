e2cefa21b502bed79b7b5762113dba177b9dcd7e
vgmiller <vanessa.g.miller@gmail.com>
2026-06-09 11:53:50 -0400

    security updates

--- vmillermusic/settings.py
+++ vmillermusic/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Vanessa Warning: I DO WHAT I WANT
 DEBUG = True
 
 ALLOWED_HOSTS = [
